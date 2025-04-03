138dc16957533245e2304020e7a159a6d96328d8
ZwierzchowskiM <marcin.zwierzchowski1@gmail.com>
2025-04-03 22:22:54 +0200

    added function for visualisation data

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,27 +1,55 @@
 import yfinance as yf
 import pandas as pd
 import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
-# Funkcja pobierająca dane
-def get_stock_data(ticker="^GSPC", start="2010-01-01", end="2024-04-01"):
+def get_stock_data(ticker="^GSPC", start="2010-01-01"):
+    end = datetime.today().strftime('%Y-%m-%d')
     print(f"Pobieranie danych dla {ticker}...")
     data = yf.download(ticker, start=start, end=end)
     
-    # Ścieżka do katalogu głównego projektu
+
     project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    # Tworzymy folder 'data' jeśli nie istnieje
     os.makedirs(os.path.join(project_dir, "data"), exist_ok=True)
-
-    # Ścieżka do zapisu pliku CSV w folderze 'data'
     file_path = os.path.join(project_dir, "data", f"{ticker}_data.csv")
 
-    # Zapisujemy dane do pliku CSV
+   
     data.to_csv(file_path)
     
     print(f"Dane zapisane w {file_path}")
     return data
 
+def visualize_stock_data(file_path):
+    
+    data = pd.read_csv(file_path)
+    print(data.columns)
+
+    print(data.head())
+    data = data.iloc[2:].reset_index(drop=True)
+    print(data.head())
+    
+    if 'Close' not in data.columns:
+        print(f"Brak kolumny 'Close' w pliku {file_path}.")
+        return
+    
+
+   
+    plt.figure(figsize=(12, 6))
+    plt.plot(data['Price'], data['Close'], label='Cena zamknięcia')
+    plt.legend()
+    plt.title('Wykres ceny zamknięcia')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+
+    
 if __name__ == "__main__":
-    df = get_stock_data()
-    print(df.head())
+    #get_stock_data()
+    file_path = "../data/^GSPC_data.csv"
+    #check_data(file_path)
+    visualize_stock_data(file_path)
